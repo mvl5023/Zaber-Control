c319b30dcfff95fdb04376bb5470467c87fa9988
mvl5023 <mvl5023@psu.edu>
2016-06-03 12:07:30 -0400

    testing serial connection
    initial code to test for availability of data over serial connection
    with zaber

--- test/test.ino
+++ test/test.ino
@@ -1,21 +1,20 @@
 #include <za_serial.h>
 
 z_port zaber1;
-string command;
-za_reply reply;
+//za_reply reply;
 
 void setup() {
-  Serial.begin(9600);
-  //za_connect(*zaber1, *COM1);     determine COM port of zaber
+  Serial.begin(9600);  
+  za_connect(zaber1, COM3);     //determine COM port of zaber
   Serial.println("Enter commands");
 }
 
 void loop() {
   if(Serial.available() > 0)
     {
-      command = Serial.read();
-      za_send(zaber1, command);
-      za_receive(zaber1, reply);
+      za_send(zaber1, Serial.read());
+      delay(100);
+      /*za_receive(zaber1, reply);
       Serial.print(reply.message_type);
       Serial.print(reply.device_address);
       Serial.print(" ");
@@ -27,6 +26,6 @@ void loop() {
       Serial.print(" ");
       Serial.print(reply.warning_flags);
       Serial.print(" ");
-      Serial.println(reply.response_data);
+      Serial.println(reply.response_data);*/
     }
 }
