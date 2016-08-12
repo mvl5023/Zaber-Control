3daa3a27c1e97e2d041119cf7d1094e334104ce5
Michael Lipski <mvl5023@psu.edu>
2016-08-12 10:51:44 -0400

    Updated Zaber_X library
    Added interp2 function that uses f80 lens data

--- Zaber_X/zaberx.h
+++ Zaber_X/zaberx.h
@@ -7,10 +7,9 @@
 #define resolutionDeg  0.000234375
 #define resolutionRad  0.000004091
 
-#define quad 4294967296  
+#define quad 4294967296
 #define cubed 16777216
 #define squared 65536
-
 
 
 	int readAnalog(int analogPin, int iterations);
@@ -24,6 +23,8 @@
 	long stepsR(float radis);
 
 	double interp1(double input);
+
+	double interp2(double input);
 
 
 #endif
